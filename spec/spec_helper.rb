c631163bb3366f2be3a03d985d6797cd464e3bc8
Ahmed Javed <ahmed.javed@gmail.com>
2015-02-02 01:07:38 -0500

    removed coveralls from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,5 @@
 require 'rspec/core'
 require 'simplecov'
-require 'coveralls'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
 
 SimpleCov.start do
   add_filter 'spec'
