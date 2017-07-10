0c100b55af96fdf23c2fd6ec121c6956cc72dead
Ahmed Javed <ahmed.javed@gmail.com>
2017-07-09 23:09:24 -0400

    Cleaned up lcd_state_machine_spec

--- spec/unit/lcd_buttons_spec.rb
+++ spec/unit/lcd_buttons_spec.rb
@@ -9,15 +9,13 @@ module RaspberryPiControlPanel
       allow(Lcd).to receive(:instance).and_return(lcd)
     end
 
-    subject { LcdButtons.send :new }
+    let(:lcd_state_machine) { instance_double LcdStateMachine }
 
     let(:logger) do
       double(Logging::Logger).tap do |logger|
         allow(logger).to receive('[]').with('LcdButtons').and_return(logger)
       end
     end
-
-    let(:lcd_state_machine) { instance_double LcdStateMachine }
 
     let!(:lcd) do
       instance_double(Adafruit::LCD::Char16x2).tap do |lcd|
@@ -27,46 +25,48 @@ module RaspberryPiControlPanel
       end
     end
 
+    it 'raises error for up_button_pressed? because up button is not used' do
+      expect { lcd_buttons.up_button_pressed? }.to raise_error(NoMethodError)
+    end
+
     RaspberryPiControlPanel::LcdButtonsSpecHelper::BUTTON_SYMBOLS.each do |button|
-      context "##{button}_button_pressed?" do
-        let(:method_name) { (button.to_s + '_button_pressed?').to_sym }
-
-        let(:lcd_constant) { get_lcd_button_constant button }
-
+      context "##{button}_button_pressed? method is dynamically defined" do
         it 'returns truthy value when pressed' do
           allow(lcd).to receive(:button_pressed).with(lcd_constant).and_return(true)
-          expect(subject.send method_name).to be_truthy
+          expect(lcd_buttons.send method_name).to be_truthy
         end
 
         it 'returns falsy value when not pressed' do
           allow(lcd).to receive(:button_pressed).with(lcd_constant).and_return(false)
-          expect(subject.send method_name).to be_falsy
+          expect(lcd_buttons.send method_name).to be_falsy
         end
+
+        let(:method_name) { (button.to_s + '_button_pressed?').to_sym }
+
+        let(:lcd_constant) { get_lcd_button_constant button }
       end
     end
 
-    it 'raises error when a method is not found' do
-      expect { subject.bogus }.to raise_error(NoMethodError)
-    end
+    subject(:lcd_buttons) { LcdButtons.send :new }
 
-    context '#which_button_was_pressed?' do
+    context '#which_button_was_pressed? method' do
       RaspberryPiControlPanel::LcdButtonsSpecHelper::BUTTON_SYMBOLS.each do |button|
         it "returns :#{button} when #{button} button is pressed" do
           allow(lcd).to receive(:button_pressed).with(get_lcd_button_constant button).and_return(true)
-          expect(subject.send :which_button_was_pressed?).to eq(button)
+          expect(lcd_buttons.send :which_button_was_pressed?).to eq(button)
         end
       end
 
       it 'returns nil when no button is pressed' do
-        expect(subject.send :which_button_was_pressed?).to be nil
+        expect(lcd_buttons.send :which_button_was_pressed?).to be nil
       end
     end
 
-    context '#monitor_buttons' do
+    context '#monitor_buttons method' do
       it 'stops monitoring when lcd state machine reaches :terminating state' do
         allow(lcd_state_machine).to receive(:state).and_return(:terminating)
         expect(logger).to receive(:debug).once.ordered.with('Monitoring buttons')
-        subject.monitor_buttons
+        lcd_buttons.monitor_buttons
       end
 
       it 'calls lcd state machine when select button is pressed' do
@@ -78,7 +78,7 @@ module RaspberryPiControlPanel
         expect(logger).to receive(:debug).once.ordered.with('Monitoring buttons')
         expect(logger).to receive(:debug).once.ordered.with('select button pressed')
 
-        subject.monitor_buttons
+        lcd_buttons.monitor_buttons
       end
     end
   end
