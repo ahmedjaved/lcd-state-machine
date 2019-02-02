62812c64de3329fb5ae04ae331064da6fc43ab14
Ahmed Javed <ahmed.javed@gmail.com>
2019-02-02 17:20:53 -0500

    m: use sonarcloud

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,11 +9,6 @@ SimpleCov.start do
   add_filter '/vendor/'
 end
 
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 require 'lcd_buttons'
 module RaspberryPiControlPanel
   module LcdButtonsSpecHelper
